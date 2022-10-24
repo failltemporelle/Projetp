5e9560260c2bded647f955d56a35c8f08fb30117
failltemporelle <benjaminruyerpro@gmail.com>
2022-10-24 23:37:18 +0200

    remove page

--- f1-app/app.vue
+++ f1-app/app.vue
@@ -1,10 +1,64 @@
 <template>
-  <bar />
-  <NuxtLink to="/">Home page</NuxtLink>
-  <points/>
+    <bar />
+    <div>
+        <button class="btn" @click="getpoints()">Points</button>
+        <button class="btn" @click="gettoken()">token</button>
+    </div>
+    <div>
+        <div class="flex flex-row flex-wrap place-content-center">
+            <div v-for="item in this.f1.points" class="card w-96 bg-base-100 shadow-xl ml-6 mt-6">
+                <div class="card-body">
+                    <p> Points: {{ item.points }}</p>
+                    <p class="font-bold">{{ item.Driver.givenName }} {{ item.Driver.familyName }}</p>
+                    <p> Position: {{ item.position }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+export default ({
+    data() {
+        return {
+            f1: {
+                driver: [],
+                points: [],
+            },
+        }
+    },
+    methods: {
+        getpoints() {
+            fetch('http://ergast.com/api/f1/2022/driverStandings.json')
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.f1.points = data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+                    console.log(this.f1.points);
+
+                })
+        },
 
 
+        gettoken() {
+            fetch('https://api.pro.wizconnected.com/api/oauth/token', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        "grant_type": "client_credentials",
+                        "client_id": "client_id",
+                        "client_secret": "client_secret"
+                    })
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.f1.token = data.access_token;
+                    console.log(this.f1.token);
 
-
-
+                })
+        },
+    }
+})
+</script>
