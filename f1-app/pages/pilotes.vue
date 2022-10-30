7aa4e8f0fd44715842203f43065e20cfb39e9a6b
failltemporelle <benjaminruyerpro@gmail.com>
2022-10-30 15:01:24 +0100

    card pilotes

--- f1-app/pages/pilotes.vue
+++ f1-app/pages/pilotes.vue
@@ -1,29 +1,45 @@
-<template data-theme="cupcake">
+<template>
   <bar />
   <div>
     <div class="flex flex-row flex-wrap place-content-center">
+      <!-- <div  v-for="item in this.f1.points" class="indicator">
+      <span class="indicator-item badge badge-primary">{{item.points}}</span>
+      <div class="card w-96 bg-primary ml-6 mt-6">
+        <div class="card-body">
+          <p>{{ item.Driver.givenName }} {{ item.Driver.familyName }}</p>
+          <p>{{ item.Driver.driverId }}</p>
+          <p>{{ item.points }} Points</p>
+          <div class="card-actions ml-6 mt-6">
+            <button class="btn">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div> -->
       <div
         v-for="item in this.f1.points"
         class="card w-60 bg-red-700 shadow-xl ml-6 mt-6"
       >
+        <span class="indicator-item badge badge-primary">{{
+          item.points
+        }}</span>
         <div class="card-body">
-          <p>{{item.Driver.givenName}} {{item.Driver.familyName}}</p>
-           <p>{{item.Driver.driverId}}</p>
+          <p>{{ item.Driver.givenName }} {{ item.Driver.familyName }}</p>
+          <p>{{ item.Driver.driverId }}</p>
           <p>{{ item.points }} Points</p>
+          <nuxt-img src="f1-app/assets/hamilton.jpg" />
+
         </div>
-        <img  src=@/assets/hamilton.jpg>
+        <!-- <nuxt-img src="require(`~/assets/img/${item.Driver.driverId}.jpg`)" /> -->
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  
-
-  created(){
-    this.getpoints()
+  created() {
+    this.getpoints();
   },
   data() {
     return {
@@ -34,7 +50,7 @@ export default {
       },
     };
   },
-  
+
   methods: {
     getpoints() {
       this.f1.ecuries = [];
@@ -49,7 +65,4 @@ export default {
     },
   },
 };
-
 </script>
-
-
