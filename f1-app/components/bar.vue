dd4d6530e1a730a12c9d7cc9bc750d88524dec7d
failltemporelle <benjaminruyerpro@gmail.com>
2022-10-30 02:30:03 +0200

    update nav bar

--- f1-app/components/bar.vue
+++ f1-app/components/bar.vue
@@ -13,7 +13,7 @@
     </div>
   </div>
   <div class="navbar-center">
-    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <a class="btn btn-ghost normal-case text-xl">Formula one</a>
   </div>
   <div class="navbar-end">
     <button class="btn btn-ghost btn-circle">
