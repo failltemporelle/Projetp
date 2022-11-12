907b54a34e7e98bd6c01da3c508dc28903364912
failltemporelle <benjaminruyerpro@gmail.com>
2022-11-12 11:19:21 +0100

    push

--- f1-app/nuxt.config.ts
+++ f1-app/nuxt.config.ts
@@ -3,10 +3,32 @@ export default defineNuxtConfig({
   targert: "static",
   ssr: false,
   modules: ['@nuxtjs/tailwindcss'],
+  
   typescript: {
     typeCheck: true,
   },
   link: [
     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
   ],
+
+  // modules: [
+  //   ['@nuxtjs/tailwindcss'],
+  //   [
+  //     '@nuxtjs/firebase',
+  //     {
+  //       config: {
+  //         apiKey: '<apiKey>',
+  //         authDomain: '<authDomain>',
+  //         projectId: '<projectId>',
+  //         storageBucket: '<storageBucket>',
+  //         messagingSenderId: '<messagingSenderId>',
+  //         appId: '<appId>',
+  //         measurementId: '<measurementId>'
+  //       },
+  //       services: {
+  //         auth: true // Just as example. Can be any other service.
+  //       }
+  //     }
+  //   ], 
+  // ]
 })
